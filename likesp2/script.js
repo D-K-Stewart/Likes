10229d8754e288add77a4ca0ef8bc93eecb4467c
D-K-Stewart <90126394+D-K-Stewart@users.noreply.github.com>
2022-01-19 15:49:43 -0800

    Initial commit

---
+++ likesp2/script.js
@@ -0,0 +1,23 @@
+var count = 1;
+var countElement = document.querySelector("#count");
+
+function add1() {
+    count++;
+    countElement.innerText = count + " like(s)";
+}
+
+var one = 1;
+var oneElement = document.querySelector("#one");
+
+function add2() {
+    one++;
+    oneElement.innerText = one + " like(s)";
+}
+
+var two = 1;
+var twoElement = document.querySelector("#two");
+
+function add3() {
+    two++;
+    twoElement.innerText = two + " like(s)";
+}
